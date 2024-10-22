73e2767a0d360145282ff0b00fe5a1b8adebb658
masx200 <34191203+masx200@users.noreply.github.com>
2024-10-22 09:22:39 +0800

    print_all_tailscale_ips.ts

--- print_all_tailscale_ips.ts
+++ print_all_tailscale_ips.ts
@@ -1,17 +1,16 @@
-import { isIPv6 } from "https://deno.land/std@0.143.0/node/internal/net.ts";
-import type { DDNScontentContent } from "./DDNScontentContent.ts";
-import { runCommand } from "./runCommand.ts";
 import {
-    getPublicIpv4,
-    getPublicIpv6,
-} from "https://deno.land/x/masx200_get_public_ip_address@1.0.4/mod.ts";
-import { isIPv4 } from "https://deno.land/std@0.169.0/node/internal/net.ts";
-/**
- * 获取所有Tailscale网络IP和自定义公共IP
- * @param opts - 选项参数
- * @returns Promise<DDNScontentContent[]>
- */
-export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(
+    isIPv4,
+    isIPv6,
+} from "https://deno.land/std@0.143.0/node/internal/net.ts";
+import type { DDNScontentContent } from "./DDNScontentContent.ts";
+import { getAllTailscaleNetworkIPsAndSelfPublicIPs } from "./get_all_tailscale_ips.ts";
+import { runCommand } from "./runCommand.ts";
+
+console.log(await getAllTailscaleNetworkIPsAndSelfPublicIPs());
+
+console.log(await getPeersTailscaleNetworkIPs());
+
+export async function getPeersTailscaleNetworkIPs(
     opts: {
         name: string;
         public: boolean;
@@ -20,21 +19,28 @@ export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(
         ipv6: boolean;
     } = { name: "", public: true, tailscale: true, ipv4: true, ipv6: true },
 ): Promise<DDNScontentContent[]> {
-    const { name, tailscale, ipv4, ipv6 } = opts;
+    const { name, tailscale /* , ipv4, ipv6  */ } = opts;
     const selfIPs: string[] = [];
     const config = name
         ? {
             [name]: selfIPs,
         }
         : {};
-
+    let data1: {
+        Self: { DNSName: string; TailscaleIPs: string[] };
+        Peer: Record<string, { DNSName: string; TailscaleIPs: string[] }>;
+    } | null = null;
     if (tailscale) {
         try {
             const text = await runCommand("tailscale", ["status", "--json"]);
             const data: {
                 Self: { DNSName: string; TailscaleIPs: string[] };
-                // Peer: Record<string, { DNSName: string; TailscaleIPs: string[] }>;
+                Peer: Record<
+                    string,
+                    { DNSName: string; TailscaleIPs: string[] }
+                >;
             } = JSON.parse(text);
+            data1 = data;
             selfIPs.push(...Array.from(data.Self.TailscaleIPs));
             const tailscale_name = tailscale
                 ? data.Self.DNSName.slice(0, -1)
@@ -44,28 +50,29 @@ export async function getAllTailscaleNetworkIPsAndSelfPublicIPs(
             console.error(error);
         }
     }
-
-    // for (const v of Object.values(data.Peer)) {
-    //     Object.assign(config, {
-    //         [v.DNSName.slice(0, -1)]: v.TailscaleIPs,
-    //     });
-    // }
-    if (opts.public) {
-        if (ipv4) {
-            try {
-                selfIPs.push(await getPublicIpv4());
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        if (ipv6) {
-            try {
-                selfIPs.push(await getPublicIpv6());
-            } catch (error) {
-                console.error(error);
-            }
+    if (data1) {
+        for (const v of Object.values(data1.Peer)) {
+            Object.assign(config, {
+                [v.DNSName.slice(0, -1)]: v.TailscaleIPs,
+            });
         }
     }
+    // if (opts.public) {
+    //     if (ipv4) {
+    //         try {
+    //             selfIPs.push(await getPublicIpv4());
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     }
+    //     if (ipv6) {
+    //         try {
+    //             selfIPs.push(await getPublicIpv6());
+    //         } catch (error) {
+    //             console.error(error);
+    //         }
+    //     }
+    // }
     // console.log(JSONSTRINGIFYNULL4(config, null, 4))
     // return config;
     const result = new Array<DDNScontentContent>();
