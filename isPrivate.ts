1ca211982361103092cbac49e52ed005642bf84d
masx200 <masx200@qq.com>
2025-02-09 22:17:57 +0800

    Update isPrivate.ts

--- isPrivate.ts
+++ isPrivate.ts
@@ -4,10 +4,10 @@ import ip from "npm:ip@2.0.1";
 export function isPrivate(a: string) {
     // deno-lint-ignore ban-ts-comment
     //@ts-ignore
-    if (ip.default.cidrSubnet("100.64.0.0/10").contains(a)) {
+    if (ip.cidrSubnet("100.64.0.0/10").contains(a)) {
         return true;
     }
     // deno-lint-ignore ban-ts-comment
     //@ts-ignore
-    return ip.default.isPrivate(a);
+    return ip.isPrivate(a);
 }
